5def31c2be8e7c4a2703b5936369a6a121b2142a
jgomez002 <jdg8858@nyu.edu>
2021-11-29 15:50:36 -0500

    New things
    setting up hehe

---
+++ FinalProjectCC.js
@@ -0,0 +1,12 @@
+
+function setup() {
+createCanvas (windowWidth, windowHeight); // using (windowWidth, windowHeight) gives me a better visual canvas to work with, which I enjoyed but it was harder to determine postion values for certain objects vs declaring a set canvas size. Neverthless I enjoyed using this canvas size
+background (0);
+ 
+
+}
+
+function draw() {
+
+}
+
